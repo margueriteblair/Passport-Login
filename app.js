25f2d621f94e8eb16eb34a585483ba5ac5ee7d84
Marguerite Blair <marguerite.blair@gmail.com>
2020-08-26 12:53:32 -0400

    new packages like passport

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express');
 const app = express();
 app.use(express.urlencoded({extended: false}))
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session')
+
+const initializePassport = require('./passport-config');
+initializePassport(passport,
+     email => {users.find(user => user.email === email)});
+
+const users = []; //not something we actually want to do in production
 
 app.set('view-engine', 'ejs')
-
+app.use(flash())
+app.use(session({secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false}))
 app.get('/', (req, res) => {
     res.render('index.ejs', {name: "Kyle"})
 })
 
+app.use(passport.initialize())
+app.use(passport.session())
 app.get('/login', (req, res)=> {
     res.render('login.ejs')
 })
@@ -15,12 +32,26 @@ app.get('/login', (req, res)=> {
 app.get('/register', (req, res)=> {
     res.render('register.ejs')
 })
-app.post('/login', (req, res) => {
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
 
-})
-
-app.post('/register', (req, res) => {
-
+app.post('/register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login');
+    } catch (error) {
+        res.redirect('/register');
+    }
+    console.log(users);
 })
 
 app.listen(3000);
